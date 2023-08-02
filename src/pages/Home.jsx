13f8c99647a13ec37922501e2110386aea5ec437
kareem shaban <shabankareem577@gmail.com>
2023-08-02 14:13:06 +0300

    render api data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,37 @@
 import { useState } from 'react';
+import get_api from '../api/shows';
 
 const Home = () => {
   const [searchStr, setSearchStr] = useState('');
+  const [movies, setMovies] = useState(null);
+  const [showsError, setShowError] = useState(null);
 
+  //console.log(movies);
   function inpValChange(ev) {
     setSearchStr(ev.target.value);
   }
   async function search(ev) {
     ev.preventDefault();
-    var res = (
-      await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
-    ).json();
-    res.then(response => {
-      console.log(response);
-    });
+
+    try {
+      setShowError(null);
+      const body = await get_api(`${searchStr}`);
+
+      setMovies(body);
+    } catch (error) {
+      setShowError(error.message);
+    }
+  }
+  function renderMovies() {
+    if (showsError) {
+      return <div>Error Eccured : ${showsError.message}</div>;
+    }
+    if (movies) {
+      return movies.map(movie => {
+        return <div key={movie.show.id}>{movie.show.name}</div>;
+      });
+    }
+    return null;
   }
 
   return (
@@ -23,6 +41,7 @@ const Home = () => {
         <input type="text" value={searchStr} onChange={inpValChange} />{' '}
         <button type="submit">ٍSearch</button>
       </form>
+      {renderMovies()}
     </div>
   );
 };
